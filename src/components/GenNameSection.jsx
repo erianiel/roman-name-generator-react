7b711fb618cd8faae43b10d0e4e180e4659540ba
erianiel <valentina.gravil@gmail.com>
2024-08-20 16:33:34 +0200

    add namecontent component in gennamesection and styling

--- src/components/GenNameSection.jsx
+++ src/components/GenNameSection.jsx
@@ -1,16 +1,18 @@
 import styles from "./GenNameSection.module.css";
 
 import Form from "./FormGen.jsx";
+import NameContent from "./NameContent.jsx";
 
 function GenNameSection() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1 className={styles.title}>
         Create your <span className={styles.highlight}>realistic</span> <br />
         Ancient Roman <span className={styles.highlight}>name!</span>
       </h1>
 
       <Form />
+      <NameContent />
     </div>
   );
 }
---
+++ src/components/NameContent.jsx
@@ -0,0 +1,31 @@
+import styles from "./NameContent.module.css";
+
+function NameInfo() {
+  return (
+    <div className={styles.container}>
+      <h3>Marcus Tullius Cicero</h3>
+      <div className={styles.contentBox}>
+        <div>
+          <h2>Status</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </p>
+        </div>
+        <div>
+          <h2>Gender</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NameInfo;
