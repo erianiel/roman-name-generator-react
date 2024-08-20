74a3cdc4b49d8ac32a11579081293edbd6541827
erianiel <valentina.gravil@gmail.com>
2024-08-20 16:37:01 +0200

    add components and styling to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,17 @@
+import PageNav from "../components/PageNav";
+import GenNameSection from "../components/GenNameSection";
+import Sidebar from "../components/Sidebar";
+import styles from "./Homepage.module.css";
+
 export default function Homepage() {
   return (
-    <main>
-      <h1>Hello!</h1>
+    <main className={styles.homepage}>
+      <PageNav />
+
+      <section>
+        <GenNameSection />
+        <Sidebar />
+      </section>
     </main>
   );
 }
