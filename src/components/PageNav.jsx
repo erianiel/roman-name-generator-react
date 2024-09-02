0deaa4d2c6e5d44f0470a370780ab2808623d3e0
erianiel <valentina.gravil@gmail.com>
2024-09-02 17:22:55 +0200

    feat: add media queries

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,15 +1,36 @@
 import { Link, NavLink } from "react-router-dom";
+import { useMediaQuery } from "@custom-react-hooks/use-media-query";
+import { useState } from "react";
+import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
+import Button from "./Button";
 
 import styles from "./PageNav.module.css";
 
 function PageNav() {
+  const isMobile = useMediaQuery("(max-width: 67em");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className={styles.nav}>
       <Link to="/">
         <h1>Roman name generator</h1>
       </Link>
 
-      <ul>
+      {isMobile && (
+        <Button onClick={() => setIsMenuOpen(!isMenuOpen)} variant="secondary">
+          {!isMenuOpen ? (
+            <IoMenuOutline size="3.2rem" />
+          ) : (
+            <IoCloseOutline size="3.2rem" />
+          )}
+        </Button>
+      )}
+
+      <ul
+        className={`${styles.list} ${
+          isMobile && !isMenuOpen ? "hidden" : styles.navOpen
+        }`}
+      >
         <li>
           <NavLink to="/howitworks">How it works</NavLink>
         </li>
